3c6aa8cca14391ae9012d9d93a06444debf9d1a7
raulDeDevWorld <proyection009@gmail.com>
2024-05-15 19:16:44 +0200

    DC

--- src/components/BottomNavigation.jsx
+++ src/components/BottomNavigation.jsx
@@ -33,31 +33,31 @@ export default function BottomNavigation({ rol }) {
 
     return <div className={`fixed w-screen left-0 bottom-0 w-screen grid h-[70px] lg:hidden grid-cols-5 bg-[#2A52BE] border-t-[5px] border-white mx-auto font-medium z-50`}>
 
-        <Button click={() => redirectHandler(`/Cliente`)} name={'Cliente'}>
+        <Button click={() => redirectHandler(`/`)} name={'Cliente'}>
             <svg className="w-11 h-11 mb-1 text-gray-600 redrounded-full  p-1" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                 <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
             </svg>
             <span className="text-[12px] text-white   ">Inicio</span>
         </Button>
-        <Button click={() => redirectHandler(`/Distribuidor/Productos`)} name={'Productos'}>
+        <Button click={() => redirectHandler(`/Calculadora`)} name={'Productos'}>
             <svg className="w-11 h-11 mb-1 text-gray-600 redrounded-full  p-1" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                 <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
             </svg>
             <span className="text-[12px] text-white   ">Calculadora</span>
         </Button>
-        <Button click={() => redirectHandler(`/Distribuidor/Agregar`)} name={'Agregar'}>
+        <Button click={() => redirectHandler(`/Tracking`)} name={'Agregar'}>
             <svg className="w-11 h-11 mb-1 text-gray-600 redrounded-full  p-1" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                 <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"></path>
             </svg>
             <span className="text-[12px] text-white   ">Tracking</span>
         </Button>
-        <Button click={() => redirectHandler(`/Distribuidor/Transacciones`)} name={'Transacciones'}>
+        <Button click={() => redirectHandler(`/Servicios`)} name={'Transacciones'}>
             <svg className="w-11 h-11 mb-1 text-gray-600 redrounded-full  p-1" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                 <path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"></path>
             </svg>
             <span className="text-[12px] text-white   " >Servicios</span>
         </Button>
-        <Button click={() => redirectHandlerWindow(`https://api.whatsapp.com/send?phone=+59169941749&text=hola%20mundo`)}>
+        <Button click={() => redirectHandlerWindow(`https://api.whatsapp.com/send?phone=+59176586948&text=hola%20Logistics%20Gear`)}>
             <span className="w-11 h-11 mb-1 text-gray-600 redrounded-full  p-1">
                 <svg width="30" height="30" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0.853564 19.7608C0.852627 23.1216 1.73763 26.4031 3.42044 29.2955L0.692627 39.178L10.8851 36.5262C13.7042 38.0491 16.8628 38.847 20.0726 38.8472H20.0811C30.6772 38.8472 39.3026 30.2917 39.3072 19.7759C39.3092 14.6802 37.3111 9.88857 33.6808 6.28361C30.0511 2.67896 25.2237 0.692755 20.0803 0.69043C9.48294 0.69043 0.858096 9.24547 0.853721 19.7608" fill="transparent" />
