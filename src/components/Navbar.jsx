96e22994958934bc77584441f1eef17676a31459
raulDeDevWorld <proyection009@gmail.com>
2024-05-18 19:45:47 +0200

    DC

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -48,21 +48,22 @@ export default function BottomNavigation({ rol }) {
 
 
 
-                {pathname !== '/Login' && pathname !== '/SignUp' && pathname !== '/Register' && <div>
-                    {
-                        user ? <button className='relative right-5 z-50 bg-[#F7BE38] p-2 px-5 rounded-[5px] border' onClick={()=>handleSignOut()}>Cerrar Sesión</button> :
-                            <button className='relative right-5 z-50 bg-[#F7BE38] p-2 px-5 rounded-[5px] border' onClick={() => router.push('/Login')}>Iniciar Sesión</button>
+                {pathname !== '/Login' && pathname !== '/SignUp' && pathname !== '/Register' &&
+                    <div>
+                        {
+                            user ? <button className='inline relative right-5 z-50 bg-[#F7BE38] p-2 px-5 rounded-[5px] border lg:hidden' onClick={() => handleSignOut()}>Cerrar Sesión</button> :
+                                <button className='relative right-5 z-50 bg-[#F7BE38] p-2 px-5 rounded-[5px] border lg:hidden' onClick={() => router.push('/Login')}>Iniciar Sesión</button>
 
-                    }
+                        }
 
-                    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" onClick={openNav} aria-controls="navbar-default" aria-expanded="false">
-                        <span className="sr-only">Open main menu</span>
-                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 17 14">
-                            <path stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
-                        </svg>
-                    </button>
+                        <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" onClick={openNav} aria-controls="navbar-default" aria-expanded="false">
+                            <span className="sr-only">Open main menu</span>
+                            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 17 14">
+                                <path stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+                            </svg>
+                        </button>
 
-                </div>
+                    </div>
 
                 }
                 <div className='relativebg-red-500 w-full flex justify-end'>
